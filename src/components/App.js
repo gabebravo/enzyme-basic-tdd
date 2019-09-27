7fe4fd2a51b700ac48d922b97edef9182c7e481d
gabe <gabe@gabes-MacBook-Pro.local>
2019-09-26 21:09:17 -0400

    Added a map for the gift list

--- src/components/App.js
+++ src/components/App.js
@@ -10,11 +10,15 @@ export default class App extends Component {
       gifts: [...prevGifts.gifts, { id: prevGifts.gifts.length + 1 }]
     }));
 
+  renderGifts = () =>
+    [...this.state.gifts].map(gift => <div key={gift.id}>{gift.id}</div>);
+
   render() {
     return (
       <div>
         <h2>Gift Giver</h2>
         <button onClick={this.addGift}>Add Gift</button>
+        {this.state.gifts && this.renderGifts(this.state.gifts)}
       </div>
     );
   }
